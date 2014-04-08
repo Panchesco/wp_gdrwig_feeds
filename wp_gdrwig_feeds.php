df4c870aeda259d2cd6dd8d553ce3673a16d053a
RW <richard@panchesco.com>
2014-04-08 15:12:56 -0700

    Added some additional settings before adding code for pulling additional feeds.

--- wp_gdrwig_feeds.php
+++ wp_gdrwig_feeds.php
@@ -50,8 +50,11 @@ if ( ! defined( 'WPINC' ) ) {
 		{
 			/** Set Defaults **/
 			add_option( 'gdrwig_settings', 
-				array(	'client_id'=>'',
+				array(	'ig_username'=>'',
+						'client_id'=>'',
 						'client_secret'=>'',
+						'feed'=>'hashtag',
+						'user_to_show'=>'',
 						'hashtag'=>'modigliani',
 						'resolution'=>'thumbnail',
 						'count'=>0));
@@ -70,6 +73,15 @@ if ( ! defined( 'WPINC' ) ) {
 		    /*2*/   'Instagram Client App Settings',
 		    /*3*/   'gdrwig_settings_section_1_callback',
 		    /*4*/   'gdrwig_settings'
+		    );
+		    
+		    // IG Username.
+		    add_settings_field(
+		    /*1*/   'ig_username',
+		    /*2*/   'Instagram Username',
+		    /*3*/   'GdrwigFeeds::ig_username_input',
+		    /*4*/   'gdrwig_settings',
+		    /*5*/   'gdrwig_settings_section_1'
 		    );
 		     
 		    // Client ID.
@@ -91,10 +103,29 @@ if ( ! defined( 'WPINC' ) ) {
 		    );
 		    
 		    
+		    // Feed
+		    add_settings_field(
+		    /*1*/   'feed',
+		    /*2*/   'Which Feed?',
+		    /*3*/   'GdrwigFeeds::feed_dropdown',
+		    /*4*/   'gdrwig_settings',
+		    /*5*/   'gdrwig_settings_section_1'
+		    );
+		    
+		    // Ig user to show
+		    add_settings_field(
+		    /*1*/   'ig_user_to_show',
+		    /*2*/   'Which User?',
+		    /*3*/   'GdrwigFeeds::ig_user_to_show_input',
+		    /*4*/   'gdrwig_settings',
+		    /*5*/   'gdrwig_settings_section_1'
+		    );
+		    
+		    
 		    // Hashtag.
 		    add_settings_field(
 		    /*1*/   'hashtag',
-		    /*2*/   'Tag',
+		    /*2*/   'Preferred Hashtag',
 		    /*3*/   'GdrwigFeeds::hashtag_input',
 		    /*4*/   'gdrwig_settings',
 		    /*5*/   'gdrwig_settings_section_1'
@@ -126,6 +157,9 @@ if ( ! defined( 'WPINC' ) ) {
 		    // Register the stylesheet.
 		    wp_register_style( 'gdrwigStylesheet', plugins_url('assets/css/gdrwig-admin.css', __FILE__) );
 		    
+		    // Register the script.
+		    wp_register_script( 'gdrwigAdminJs', plugins_url('assets/js/gdrwig-admin.js', __FILE__) );
+		    
 		}
 
 
@@ -142,6 +176,14 @@ if ( ! defined( 'WPINC' ) ) {
  
 			}
 			
+			
+		/** IG Username Input **/
+		function ig_username_input() {
+		
+			$option = get_option('gdrwig_settings');
+		 
+		    echo( '<input type="text" name="gdrwig_settings[ig_username]" id="gdrwig_settings[ig_username]" value="' . $option['ig_username']  .'" />' );
+		}
 			
 		/** Client ID Input **/
 		function client_id_input() {
@@ -189,6 +231,37 @@ if ( ! defined( 'WPINC' ) ) {
 		    
 		}
 		
+		/** Feed **/
+		function feed_dropdown() {
+		 
+		 	$html		= '<select name="gdrwig_settings[feed]" id="gdrwig_settings[feed]">
+		 	';
+		    $opts		= get_option('gdrwig_settings');
+		    
+		    $options	= array('self'=>'Mine','user'=>'User','hashtag'=>'Hashtag','popular'=>'Popular');
+		    foreach($options as $key=>$row)
+		    {
+			   	$selected = ($opts['feed']==$key) ? ' selected':''; 
+			   	$html.=		'	<option value="' . $key . '"' . $selected . '>' . $row . '</option>
+			   	';
+		    }
+		    
+		    $html.= '</select>
+		    ';
+		    
+		    echo $html;
+		    
+		}
+		
+		
+		/** IG User to show Input **/
+		function ig_user_to_show_input() {
+		
+			$option = get_option('gdrwig_settings');
+		 
+		    echo( '<input type="text" name="gdrwig_settings[ig_user_to_show]" id="gdrwig_settings[ig_user_to_show]" value="' . $option['ig_user_to_show']  .'" />' );
+		}
+		
 		
 		/** Count **/
 		function count_input() {
@@ -204,40 +277,37 @@ if ( ! defined( 'WPINC' ) ) {
 		    ?>
 		     
 		    <div class="wrap">
-		
-		 
-		     <h2>GDRWIG</h2>
-		     <p>Some text describing what the plugin settings do.</p>
-		      
-		     <form method="post" action="options.php">
-		 
-		      <?php
-		       
-		      // Output the settings sections.
-		      do_settings_sections( 'gdrwig_settings' );
-		 
-		      // Output the hidden fields, nonce, etc.
-		      settings_fields( 'gdrwig_settings_group' );
-		 
-		      // Submit button.
-		      submit_button();
-		       
-		      ?>
-		 
-		     </form>
-		    </div>
-		    
-		    <style>
-
-		    </style>
-		
-			<div class="thumbs-wrapper">
-		    <?php
-		    
-		    echo GdrwigFeeds::tagThumbs();
-		    
-		    ?>
-			</div>
+				
+				 
+				     <h2>GDRWIG</h2>
+				     <p>Some text describing what the plugin settings do.</p>
+				      
+				     <form method="post" action="options.php">
+				 
+				      <?php
+				       
+				      // Output the settings sections.
+				      do_settings_sections( 'gdrwig_settings' );
+				 
+				      // Output the hidden fields, nonce, etc.
+				      settings_fields( 'gdrwig_settings_group' );
+				 
+				      // Submit button.
+				      submit_button();
+				       
+				      ?>
+				 
+				     </form>
+				
+				
+				    <div class="thumbs-wrapper">
+				    <?php
+				    
+				    echo GdrwigFeeds::tagThumbs();
+				    
+				    ?>
+				    </div><!-- /.thumbs-wrapper -->
+			</div><!-- /.wrap -->
 		    <?php
 
 		}
@@ -282,10 +352,21 @@ if ( ! defined( 'WPINC' ) ) {
 		
 		public static function adminStyles() 
 		{
-       /*
+        
+        /*
         * It will be called only on your plugin admin page, enqueue our stylesheet here
         */
 			wp_enqueue_style( 'gdrwigStylesheet' );
+	   	}
+	   	
+	   	
+	   	public static function adminJs() 
+		{
+        
+        /*
+        * It will be called only on your plugin admin page, enqueue our script here
+        */
+			wp_enqueue_script( 'gdrwigAdminJs');
 	   	}
 		
 
@@ -299,6 +380,7 @@ if ( ! defined( 'WPINC' ) ) {
 add_action( 'admin_menu', array('GdrwigFeeds','settingsMenu' ));
 add_action( 'admin_init', array('GdrwigFeeds','Init'));
 add_action( 'admin_init', array('GdrwigFeeds','adminStyles'));
+add_action( 'admin_init', array('GdrwigFeeds','adminJs'));
 add_filter('plugin_action_links', 'GdrwigFeeds::actionLinks', 10, 2);
 
 
